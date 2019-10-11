895c9bbc02dc113101ef0ffddc5a0e86d2734ee5
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-11 18:53:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,10 +11,10 @@ end
 
 def reduce(array, starting_point=nil)
   if starting_point
-    value = starting_point
+    accum = starting_point
     i = 0 
   else
-    value = array[0]
+    accum = array[0]
     i = 1
   end
   while i < array.length do 
