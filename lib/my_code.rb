66b4553a8e69d6e7152fbca9105717f18b8949ec
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-11 18:37:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,6 +3,8 @@ def map(array)
   new_array = []
   counter = 0 
   while counter < array.length do 
-    
+    new_array.push(yield(array[counter]))
+    counter += 1
   end
+  return new_array
 end
